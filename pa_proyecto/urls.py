c7e5da3fbecbbdb63d284db2e6d8a3a33fea9d57
nemisgerry <gerrymaths@gmail.com>
2016-10-02 18:52:03 -0500

    Register Users

---
+++ pa_proyecto/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from login.views import *
+from django.contrib.auth.views import login
+from django.contrib.auth import authenticate
+from login.views import *
+
+urlpatterns = [
+        
+   
+    url(r'^home/$', home),
+    url(r'^accounts/login/$', login),
+    url(r'^logout/$', logout_page),
+    url(r'^register/$', register), 
+    url(r'^register/success/$', register_success),
+    url(r'^admin/', include(admin.site.urls)),
+
+]
